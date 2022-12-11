e58dcf4e8994561db7b7eebbc61b018f368a543a
Yummy-Cookie-S <cookie2333265@outlook.com>
2022-12-11 11:55:00 +0800

    精简依赖为 os 一个

--- code/Python/RFRN.py
+++ code/Python/RFRN.py
@@ -1,21 +1,20 @@
-import glob
 import os
-import shutil
 
 # 获取自定义需重命名的目录以及文件
-input_dir = input("请输入需要重命名的文件夹路径：")
-input_ext = input("请输入需要重命名的文件后缀：")
-input_name = input("请输入文件类型：")
+target_dir = input("需要处理的文件夹路径：")
+target_ext = input("需要重命名的文件的后缀：")
+file_type = input("文件类型：")
 
 # 获取当前目录中，以input_ext为扩展名的文件
-filenames = glob.glob(os.path.join(input_dir, "*." + input_ext))
-format_str = input_name + "_%03d." + input_ext
+walker = os.walk(target_dir)
+format_str = file_type + "_%03d." + target_ext
 
-# 遍历文件列表，重命名每个文件
-for i, filename in enumerate(filenames):
-  # 使用 format_str 格式化文件名
-  new_name = format_str % (i + 1)
-  # 重命名文件
-  os.rename(filename, new_name)
-  # 移动文件到input_dir文件夹中
-  shutil.move(new_name, input_dir)
+for path,dir_list,file_list in walker:
+  for i, filename in enumerate(file_list):
+    if filename.endswith(target_ext):
+      # 使用 format_str 格式化文件名
+      new_name = format_str % (i + 1)
+      # 重命名文件
+      os.rename(filename, new_name)
+      # 移动文件到input_dir文件夹中
+      os.rename(new_name, os.path.join(target_dir, new_name))
