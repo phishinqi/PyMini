866adf4e80f02692f388ca22535e58a9dbf2f04d
Ryokou-Kiryu <86942867+phishinqi@users.noreply.github.com>
2023-01-04 22:01:34 +0800

    Update PyTrackers_New.py

--- code/Python/PyTrackers/PyTrackers_New.py
+++ code/Python/PyTrackers/PyTrackers_New.py
@@ -1,24 +1,27 @@
-#增加了main_url的判斷
+#增加了main_url的判断
+# coding=utf-8
 import urllib.request
 import os
 
 
 
 url_file_path = os.getcwd()
-print("當前文件路徑：", url_file_path)
+print("当前文件路径：", url_file_path)
 url_path = url_file_path + '\\main_url.txt'
 if os.path.exists(url_path):
     print("OK!")
     print("--------------------")
 else:
-    print("文件不存在！正在創建中......")
-    os.mkdir(url_path)
+    print("文件不存在！正在创建中......")
+    file_url = open('./main_url.txt','w')
+    file_url.write('https://newtrackon.com/api/all')
+    #os.mkdir(url_path)
     print("OK!")
     print("--------------------")
 
 
 
-print("讀取URL中......")
+print("读取URL中......")
 result = []
 with open('main_url.txt', 'r') as f:
     for line in f:
@@ -33,16 +36,17 @@ print("--------------------")
 file_path = os.getcwd()
 #print("當前文件路徑：", file_path)
 path = file_path + '\\trackers.txt'
-print("正在輸出trackers至trackers.txt")
+print("正在输出trackers至trackers.txt")
 if os.path.exists(path):
-    print("文件存在！正在刪除原始内容......")
+    print("文件存在！正在删除原始内容......")
     with open("./trackers.txt", 'r+') as files:
         files.truncate(0)
     print("OK!")
     print("--------------------")
 else:
-    print("文件不存在！正在創建中......")
-    os.mkdir(path)
+    print("文件不存在！正在创建中......")
+    file_trackers = open('./trackers.txt','w')
+    #os.mkdir(path)
     print("OK!")
     print("--------------------")
 
@@ -54,7 +58,7 @@ else:
 #打開網頁
 #print("正在嘗試獲取trackers......")
 trackers_list = []
-for url_line in open("main_url.txt"):
+for url_line in open("./main_url.txt"):
     print(url_line)
     try:
         #html = urllib.request.urlopen(url_line).read().decode()
@@ -70,6 +74,3 @@ for url_line in open("main_url.txt"):
     except:
         continue
 #print(html)
-
-
-
